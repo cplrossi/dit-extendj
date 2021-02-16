39ae5a38f42ab126f7655ff1d16061f4d8007d57
cplrossi <cplrossi@airmail.cc>
2021-02-16 16:12:33 +0100

    avanti

---
+++ testfiles/org/cplrossi/dit/ArrayDeclTest.java
@@ -0,0 +1,7 @@
+package org.cplrossi.dit;
+
+public class ArrayDeclTest {
+  void m() {
+    Class[] c = {Object.class};
+  }
+}
--- testfiles/org/cplrossi/dit/IntercastDesugaring.java
+++ testfiles/org/cplrossi/dit/IntercastDesugaring.java
@@ -3,17 +3,14 @@ package org.cplrossi.dit;
 public class IntercastDesugaring {
   public static void main(String[] args) {
     C c = new C();
+    I i = (I & J) () -> System.out.println("I'm a lambda");
 
-    /* tmp: triggering some internal machinery */
-    I i = (I & J) () -> System.out.println("I'm I&J"); // triggers syntethic class I&J building
-    ((I & J) i).m(); // triggers intersection runtime loading
+    doSomething(c);
+    doSomething(i);
+  }
 
-    /* this is: ((I & J) c).m() */
-    (($IJ) IntersectionProxy.newInstance(
- 		 										c,
- 		 										new Class[] {
- 			 									$IJ.class
- 												}
- 											)).m();
+  public static void doSomething(I i) {
+    ((I & J) i).m();
+    ((I & J) i).n();
   }
 }
